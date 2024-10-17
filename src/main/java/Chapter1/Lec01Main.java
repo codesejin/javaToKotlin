380374550e5f171fe6b50a089dcc75253fc7b7ac
sejin park <seijin0722@naver.com>
2024-10-17 23:09:49 +0900

    Lec01 : 코틀린에서 변수를 다루는 방법

---
+++ src/main/java/Chapter1/Lec01Main.java
@@ -0,0 +1,19 @@
+package Chapter1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Lec01Main {
+	public static void main(String[] args) {
+		long number1 = 10L; // (1)
+		final long number2 = 10L; // (2)
+
+		Long number3 = 1_000L; // (3)
+		Person person = new Person("최태현"); // (4)
+
+		// Collection은 final로 선언해도 내부의 데이터를 변경할 수 있다.
+		final List<Integer> numbers = new ArrayList<>(); // (5)
+		numbers.add(3);
+	}
+}
---
+++ src/main/java/Chapter1/main.kt
@@ -0,0 +1,29 @@
+package Chapter1
+
+fun main () {
+    // 가변 변수(발..)
+    var number1 = 10L
+    // 위의 코드와 동일 var number1: Long = 10L
+
+    // 불변 변수(밸..)
+    val number2 = 10L
+    // 위의 코드와 동일 val number2: Long = 10L
+
+    // 변수의 타입을 명시적으로 지정하지 않아도 자동으로 타입을 추론
+
+    // 초기값을 지정해주지 않는 경우 컴파일 에러 발생
+    var number : Int
+    //println(a) // 컴파일 에러 발생, Variable 'a' must be initialized
+
+    val list = arrayListOf(1, 2, 3, 4, 5)
+    list.add(6)  // 정상 작동
+
+    // 모든 변수는 우선 val로 만들고 꼭 필요한 경우 var로 변경한다.
+
+    // 코틀린에서 null을 허용하려면 변수 타입에 ?를 붙여야 한다.
+    var nullable : String? = null
+    var nullLong : Long? = null
+
+    // 객체 인스턴스화 할때 new 키워드 없이 객체 생성
+    var person = Person("홍길동");
+}
